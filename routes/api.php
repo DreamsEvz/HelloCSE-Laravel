23102b98b0f13899a45ce04b3fa5503dea30a626
evan <marcel.evan2001@gmail.com>
2024-09-24 15:15:02 +0200

    add auth middleware to apiRessources routes

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\AuthController;
 //Route permettant de récupérer les profils actifs qui n'est pas protégée par le middleware d'auth
 Route::get('profiles/active', [ProfileController::class, 'getActiveProfiles']);
 //Permet de générer automatiquement les routes pour un CRUD complet, celles-ci sont protégées par le middleware d'auth
-Route::apiResource('profiles', ProfileController::class);
+Route::apiResource('profiles', ProfileController::class)->middleware('auth:sanctum');
 
 //Routes d'auth
 Route::post('register', [AuthController::class, 'register']);
